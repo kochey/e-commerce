27d84817c8d492fad46e249c850956438bd552d5
--global <eromoseleochie49@gmail.com>
2026-09-23 16:01:37 +0100

    change the id formatting in cart and cartmodel

--- back_end/model/cartModel.js
+++ back_end/model/cartModel.js
@@ -1,14 +1,44 @@
+// const mongoose = require("mongoose");
+// const  schema  = require("./productModel");
+
+
+// console.log(schema);
+
+// const cartSchema = new mongoose.Schema({
+//   title: String,
+//   price: Number,
+//   description: String,
+//   category: String,
+//   quantity: Number,
+// });
+
+// const cart = mongoose.model("cart", cartSchema);
+// module.exports = cart;
+
+
 const mongoose = require("mongoose");
-const { schema } = require("./productModel");
 
-const cartSchema = new mongoose.Schema
-({
-    title: String, 
-    price: Number,
-    description: String,
-    category: String,
-    quantity : Number
-})
+const schema = require("./productModel");
 
-const cart = mongoose.model('cart', cartSchema);
-module.exports = cart
+
+const cartSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
+  },
+
+  title: String,
+
+  price: Number,
+
+  description: String,
+
+  category: String,
+
+  quantity: Number
+});
+
+const cart = mongoose.model("cart", cartSchema);
+
+module.exports = cart;
+
--- back_end/route/cart.js
+++ back_end/route/cart.js
@@ -1,126 +1,135 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const product = require('./product');
-const product_Array = require('./constant');
-const cartModel = require('../model/cartModel')
-const productModel = require('../model/productModel')
+const product = require("./product");
+const product_Array = require("./constant");
+const cartModel = require("../model/cartModel");
+const productModel = require("../model/productModel");
 
-cart_array=[{}];
-
+cart_array = [{}];
 
 // mongoose.connect(
 //   "mongodb+srv://kochey:UeaEGUhKErRYmpGu@cluster0.224lwek.mongodb.net/?appName=Cluster0"
 // )
 // .then(()=> console.log("db Connection successfull to cart"))
-// .catch((err)=>console.log(err));        
-        
+// .catch((err)=>console.log(err));
 
- router.post("/add", async (req, res) => {
-   const productId = req.body.productId
-   console.log(productId);
-   
-   
-   // const foundItem = await product.foundbyid(item => item.id == req.params.id);
-   const foundItem = await productModel.findById(productId)
-   console.log(foundItem)
+router.post("/add", async (req, res) => {
+  const productId = req.body.productId;
+  //  console.log(productId);
 
-    try{
+  // const foundItem = await product.foundbyid(item => item.id == req.params.id);
+  const foundItem = await productModel.findById(productId);
+  //  console.log(foundItem)
 
-      if (!foundItem) {
-          return res.status(404).json({
-              message: "product not found"
-          });
-      }
+  try {
+    if (!foundItem) {
+      return res.status(404).json({
+        message: "product not found",
+      });
+    }
 
-      const cartItem = new cartModel({
-      title: foundItem.title, 
+    const existingCartItem = await cartModel.findOne({
+      productId: productId,
+    });
+
+    if (existingCartItem) {
+      existingCartItem.quantity += 1;
+      await existingCartItem.save();
+      return res
+        .status(200)
+        .json({
+          message: "Product quantity increased",
+          product: existingCartItem,
+        });
+    }
+
+    const cartItem = new cartModel({
+      productId: foundItem._id,
+      title: foundItem.title,
       price: foundItem.price,
       description: foundItem.description,
       category: foundItem.category,
-      quantity : foundItem.quantity
-      });
-      await cartItem.save();
+      quantity: foundItem.quantity,
+    });
+    await cartItem.save();
 
-    
-      return res.status(200).json({
-          message: "Product found and added to cart",
-          product : foundItem 
-      });
-
-    }catch (e) {
-        console.log(e);
-        return res.status(404).json({ 
-            message: "Prodcut doesnt exist" 
-        });
-    }
-    
-      
-    
+    return res.status(200).json({
+      message: "Product found and added to cart",
+      product: foundItem,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(404).json({
+      message: "Something went wrong",
+    });
+  }
 });
 
-router.get('/add/:id', async (req, res) => {
+router.get("/add/:id", async (req, res) => {
   try {
-    const cart = await product.find({ id: req.params.id });
-    res.json(cart);
+    const cart = await product.findOne({ productId: req.params.id });
+
+    if (!cart) {
+      return res.status(404).json({
+        message: "product not in cart",
+      });
+    }
+
+    res.status(200).json(cart);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.delete("/remove", async (req,res) => {
+router.delete("/remove", async (req, res) => {
+  const productId = req.body.productId;
 
-    const productId = req.body.productId
+  try {
+    const deleteItem = await cartModel.findByIdAndDelete({
+      productId: productId,
+    });
 
-    try{
-        const deleteItem = await cartModel.findByIdAndDelete(productId)
-
-      
-
-        if(!deleteItem){
-          res.status(404).json({
-            message:"item not in cart"
-          })
-        }
-
-      res.status(200).json({
-        message:"Item successfully deleted from cart",
-        product : deleteItem
-      })
-
+    if (!deleteItem) {
+      res.status(404).json({
+        message: "item not in cart",
+      });
     }
-    catch(e){
-      console.log(e);
-      res.status(500).json({message:"Something went wrong"})
 
+    return res.status(200).json({
+      message: "Item successfully deleted from cart",
+      product: deleteItem,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+router.put("/update", async (req, res) => {
+  const productId = req.body.productId;
+  const quantity = req.body.quantity;
+
+  try {
+    updatedquantity = await cartModel.findByIdAndUpdate(
+      { productId: productId },
+      { quantity: quantity },
+      { new: true },
+    );
+
+    if (!updatedquantity) {
+      res.status(404).json({ message: "Product not found" });
     }
- })
 
-router.put("/update", async (req,res) => {
-  
-const productId = req.body.productId
-
-try{
- 
-updatedquantity = await cartModel.findByIdAndUpdate(productId, req.body.quantity)
-
-if(!updatedquantity){
-  res.status(404).json({message:"Product not found"})
-}
-
-res.status(200).json({
-  message:"Product successfully updated",
-  product : updatedquantity
-})
-
-
-}catch(e){
-  console.log(e);
-  res.status(500).json({message:"Something went wrong"})
-}
-
-})  
-
+    res.status(200).json({
+      message: "Product successfully updated",
+      product: updatedquantity,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
 
 module.exports = router;
